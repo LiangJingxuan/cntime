95c14fc05dbd3d0a4483d672fd1099e461c51eac
LiangJingXuan <xuan03121994@163.com>
2022-08-22 13:24:43 +0800

    配置vant;rem;

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import 'lib-flexible/flexible' //移动端适配
+import "./assets/css/normalize.css"
+
 import router from './router'
 import App from './App.vue'
 
